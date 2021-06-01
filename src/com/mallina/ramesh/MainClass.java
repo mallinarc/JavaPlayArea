7cc0ebd2b15a08f5529bb132bb85a9fc86f56e5f
mallinarc <mallinarc@hotmail.com>
2021-06-01 14:20:41 -0400

    First commit

---
+++ src/com/mallina/ramesh/MainClass.java
@@ -0,0 +1,30 @@
+package com.mallina.ramesh;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// initialize and read from List of integer type without generic.
+		List myIntList = new LinkedList();
+		myIntList.add(Integer.valueOf(0));
+		Integer x = (Integer) myIntList.iterator().next();
+		
+		// above code can be written as below with generics
+		List<Integer> myGenericIntList = new LinkedList();
+		myGenericIntList.add(Integer.valueOf(0));
+		Integer y = myGenericIntList.iterator().next();
+		
+		 final ArrayList<String> alphabets = new ArrayList<>(Arrays.asList("A", "B", "A", "D", "A"));
+		 alphabets.add("E");
+		 ArrayList<String> alphabets1 = new ArrayList<>();
+		 alphabets.removeAll(Collections.singleton("A"));
+		 System.out.println(alphabets);
+	}
+
+}
+ 
